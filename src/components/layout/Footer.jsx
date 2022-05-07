a68ce591aae657e5f74c3bc85acab5364bc856b7
Mir Sameer <mir.sameer@example.com>
2022-05-07 13:31:37 +0530

    Added about section

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,16 @@
 function Footer() {
   return (
     <div className="mx-auto">
-        This is my footer which needs some styling
-        {/* todo footer  */}
+      <p className="font-mono">
+        {" "}
+        Created by{" "}
+        <a className="text-lg text-white" href="https://github.com/msi117">
+          Mir Sameer{" "}
+        </a>{" "}
+        &copy; 2022{" "}
+      </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-
+import { Link } from "react-router-dom";
 function About() {
   return (
     <div>
-        <h1 className="text-6xl mb-4">Github Finder</h1>
-        {/* @todo do About on your own */}
+      <div>
+        <Link to="/ " className="btn btn-ghost btn-outline">
+          Back to Home
+        </Link>
+      </div>
+      <h1 className="text-6xl text-sky-50 my-5">About</h1>
+      <p className="m-5">
+        This site is created by Mir Sameer. <br />
+        Tutorial from Traversy media!
+      </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
